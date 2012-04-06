47d11e1c00bb088a2480e2e49065f3db60b42fa8
Joy <jming@college.harvard.edu>
2012-04-05 21:44:36 -0400

    Create compare.py
    This should work for all but 32.mid

---
+++ compare.py
@@ -0,0 +1,35 @@
+######################## COMPARE.PY ########################
+
+# Keeps track of each note combination
+# Starts with order of 2
+# This works for all except 32.mid
+
+# Array of counts
+count = {}
+
+# File number
+i = 0
+
+# Loop through each file
+while i < 60: 
+
+	# Initialize variables
+	note = notes[i]
+	length = len(note)
+	# Note position in array
+	x = 0
+
+	# Loop through each note
+	while x < length - 1: 
+		# establish pitch names
+		a = note[x].pitch.name
+		b = note[x+1].pitch.name
+
+		# Store as pair in array
+		if (a,b) in count:
+			count[a,b] = count[a,b] + 1
+		else:
+			count[a,b] = 1
+		x += 1
+		
+	i += 1
