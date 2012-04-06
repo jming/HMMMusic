e3d0bf1882b7b2e84d75a2f866be4cf89156f5da
Joy <jming@college.harvard.edu>
2012-04-05 21:38:26 -0400

    Updated array.py
    Should work for all files except 35.mid

--- array.py
+++ array.py
@@ -1,10 +1,29 @@
 # This file helps to create arrays of all the notes in each midi file
 
 # Initializes associative arrays for notes
+
 notes = {}
-i = 0
+def appendNote(i):
+	s = converter.parse('%i.mid' % (i))
+	notes[i] = []
+	measures = s[0].measures(1,60)
+	for measure in measures:
+		for note in measure.notes:
+			notes[i].append(note)
+
+pitches = {}
+def appendPitch(i):
+	s = converter.parse('%i.mid' % (i))
+	pitches[i] = []
+	measures = s[0].measures(1,60)
+	for measure in measures:
+		for note in measure.notes:
+			pitches[i].append(note.pitch)
 
 while i < 60: 
+
+	append(i)
+	i += 1
 
 	# Converts the specific midi file
 	s = converter.parse('%i.mid' % (i))
