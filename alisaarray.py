25140555858d93b8c3fae13869d0867da95b9875
alisan16 <alisan011@gmail.com>
2012-04-07 00:50:05 -0400

    alisatest

---
+++ alisaarray.py
@@ -0,0 +1,34 @@
+######################## ARRAY.PY #########################
+#                 CS51 Final Project 2012                 #
+########### Alisa Nguyen, Joy Ming, Lee Seligman ##########
+                                            
+# Create associative array of all pitches in each midi    #
+# This works for all files except 35.mid                  #
+
+from music21 import *
+
+# Initialize associative array
+notes = {}
+# Initialize array of streams for each file
+s = {}
+
+# Appends for given file i
+def appendNote(i):
+	
+	# Initialize array for key i
+	notes[i] = []
+	# Parse file given music21 function
+	s = converter.parse('%i.mid' % (i))
+	# Access measures for file i
+	measures = s[i].measures(1,60)
+
+	# Loop through each measure
+	for measure in measures:
+		# Add each note to the array at key i
+		for note in measure.notes:
+			notes[i].append(note)
+
+# Loops through all 60 files
+while i < 60:
+	appendNote(i)
+	i ++
